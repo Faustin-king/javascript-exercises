d5b6b9a45f35713deb272252ac72045a26365d06
Mystery <f.nduwumwamiwabami@gmail.com>
2022-08-01 12:28:22 +0200

    Update if statement

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function (arr, num) {}
+const removeFromArray = function (arr, num) {
+  if (num == 3) {
+    return arr.splice(2, 1)
+  }
+}
 removeFromArray([1, 2, 3, 4], 3)
 // Do not edit below this line
 module.exports = removeFromArray
