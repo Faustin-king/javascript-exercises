2aa0604dd7c2286fd9bcdf60759a7023218c48eb
Mystery <f.nduwumwamiwabami@gmail.com>
2022-08-01 16:12:51 +0200

    Update function expression

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
+const sumAll = function (...theArgs) {
+  const total = 0
+  for (const arg of theArgs) {
+    total += arg
+  }
+  return total
+}
 
-};
+console.log(total(1, 2, 3, 4))
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
