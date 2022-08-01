1747c20994478900894e00651f1c230a20431d34
Mystery <f.nduwumwamiwabami@gmail.com>
2022-08-01 12:19:13 +0200

    Add function expression

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function (arr) {
+  let removed = arr.splice(2, 0)
+  return removed
+}
+removeFromArray([1, 2, 3, 4], 3)
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
