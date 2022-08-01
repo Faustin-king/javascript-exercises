8cb2b4a49b1cb9ee41394d858cf08f178e57cd53
Mystery <f.nduwumwamiwabami@gmail.com>
2022-08-01 14:56:32 +0200

    Delete function expression

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,4 @@
-const removeFromArray = function (arr, num) {
-  if (num == 3) {
-    return arr.splice(2, 1)
-  }
-}
-removeFromArray([1, 2, 3, 4], 3)
+const removeFromArray = function () {}
+
 // Do not edit below this line
 module.exports = removeFromArray
