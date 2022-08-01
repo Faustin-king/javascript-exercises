4e5b9acdd4574d55c70327c0aff4dd5ae6b791c6
Mystery <f.nduwumwamiwabami@gmail.com>
2022-08-01 12:22:42 +0200

    Add num argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,4 @@
-const removeFromArray = function (arr) {}
+const removeFromArray = function (arr, num) {}
 removeFromArray([1, 2, 3, 4], 3)
 // Do not edit below this line
 module.exports = removeFromArray
