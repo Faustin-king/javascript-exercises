9437b8d6b6fe48ebcb94113c5a879e57387686bf
Mystery <f.nduwumwamiwabami@gmail.com>
2022-08-01 18:39:28 +0200

    Update if statement

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
-};
+const leapYears = function (leapYear) {
+  if (leapYear / 4 && leapYear / 400) {
+    return true
+  } else {
+    return false
+  }
+}
 
 // Do not edit below this line
-module.exports = leapYears;
+module.exports = leapYears
