8b6f1295b1e5a517d81c9746faab28a2326ec5b0
Mystery <f.nduwumwamiwabami@gmail.com>
2022-08-01 17:36:02 +0200

    Update function expression

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function () {}
+const sumAll = function (min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR'
+  if (min < 0 || max < 0) return 'ERROR'
+  if (min > max) {
+    const number = min
+    min = max
+    max = number
+  }
+
+  let total = 0
+  for (let i = min; i < max + 1; i++) {
+    total += i
+  }
+  return total
+}
 
 // Do not edit below this line
 module.exports = sumAll
