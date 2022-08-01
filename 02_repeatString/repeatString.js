01bd8215d109ddbb920f1971971fd8144cd714ab
Mystery <f.nduwumwamiwabami@gmail.com>
2022-08-01 08:42:05 +0200

    Add function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function (string, num) {
+  let result = ''
+  for (i = 0; i < num; i++) {
+    result += string
+  }
+  return result
+}
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString
