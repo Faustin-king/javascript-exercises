54db4de5267e88dd54271c11489ba7e386379e4c
Mystery <f.nduwumwamiwabami@gmail.com>
2022-08-01 12:20:55 +0200

    Delete removed variable

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,4 @@
-const removeFromArray = function (arr) {
-  let removed = arr.splice(2, 0)
-  return removed
-}
+const removeFromArray = function (arr) {}
 removeFromArray([1, 2, 3, 4], 3)
 // Do not edit below this line
 module.exports = removeFromArray
